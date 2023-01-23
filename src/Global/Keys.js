8bec5527d2cfcfdd95af85b53c77ca0be743bff0
Michael Ajayi <ajayimichael@hotmail.com>
2023-01-23 13:28:08 +0100

    Changed to new onRender API

--- src/Global/Keys.js
+++ src/Global/Keys.js
@@ -1,5 +1,5 @@
 const api = "http://localhost:5001/api";
 // const remoteApi = "https://mainnet-dapps-api.herokuapp.com/api";
-const remoteApi = "https://mainnetonline-netlify-api.onrender.com/api";
+const remoteApi = "https://mainnetonline-netlify-api-guiq.onrender.com/api";
 
 module.exports = { api, remoteApi };
